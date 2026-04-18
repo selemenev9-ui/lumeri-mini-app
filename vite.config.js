66f9a40f82605724e6999683b254632688230a7d
selemenev9-ui <selemenev9@gmail.com>
2026-04-18 15:59:16 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/lumeri-mini-app/',
   build: {
     outDir: 'dist',
     assetsInlineLimit: 0
